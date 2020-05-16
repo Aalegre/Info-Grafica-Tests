f0b48b028e84f3051dc906453b7ff5de3130005b
Aalegre Desktop <alberto.alegre@enti.cat>
2020-05-16 18:34:02 +0200

    Transformacion dependiente del objeto

--- code/src/render.cpp
+++ code/src/render.cpp
@@ -1,6 +1,7 @@
 #include <GL\glew.h>
 #include <glm\gtc\type_ptr.hpp>
 #include <glm\gtc\matrix_transform.hpp>
+#include <glm/gtx/quaternion.hpp>
 #include <cstdio>
 #include <cassert>
 #include <string>
@@ -318,129 +319,6 @@ namespace Axis {
 	}
 }
 
-////////////////////////////////////////////////// CUBE
-namespace Cube {
-	GLuint cubeVao;
-	GLuint cubeVbo[3];
-	GLuint cubeShaders[2];
-	GLuint cubeProgram;
-	glm::mat4 objMat = glm::mat4(1.f);
-
-	extern const float halfW = 0.5f;
-	int numVerts = 24 + 6; // 4 vertex/face * 6 faces + 6 PRIMITIVE RESTART
-
-						   //   4---------7
-						   //  /|        /|
-						   // / |       / |
-						   //5---------6  |
-						   //|  0------|--3
-						   //| /       | /
-						   //|/        |/
-						   //1---------2
-	glm::vec3 verts[] = {
-		glm::vec3(-halfW, -halfW, -halfW),
-		glm::vec3(-halfW, -halfW,  halfW),
-		glm::vec3(halfW, -halfW,  halfW),
-		glm::vec3(halfW, -halfW, -halfW),
-		glm::vec3(-halfW,  halfW, -halfW),
-		glm::vec3(-halfW,  halfW,  halfW),
-		glm::vec3(halfW,  halfW,  halfW),
-		glm::vec3(halfW,  halfW, -halfW)
-	};
-	glm::vec3 norms[] = {
-		glm::vec3(0.f, -1.f,  0.f),
-		glm::vec3(0.f,  1.f,  0.f),
-		glm::vec3(-1.f,  0.f,  0.f),
-		glm::vec3(1.f,  0.f,  0.f),
-		glm::vec3(0.f,  0.f, -1.f),
-		glm::vec3(0.f,  0.f,  1.f)
-	};
-
-	glm::vec3 cubeVerts[] = {
-		verts[1], verts[0], verts[2], verts[3],
-		verts[5], verts[6], verts[4], verts[7],
-		verts[1], verts[5], verts[0], verts[4],
-		verts[2], verts[3], verts[6], verts[7],
-		verts[0], verts[4], verts[3], verts[7],
-		verts[1], verts[2], verts[5], verts[6]
-	};
-	glm::vec3 cubeNorms[] = {
-		norms[0], norms[0], norms[0], norms[0],
-		norms[1], norms[1], norms[1], norms[1],
-		norms[2], norms[2], norms[2], norms[2],
-		norms[3], norms[3], norms[3], norms[3],
-		norms[4], norms[4], norms[4], norms[4],
-		norms[5], norms[5], norms[5], norms[5]
-	};
-	GLubyte cubeIdx[] = {
-		0, 1, 2, 3, UCHAR_MAX,
-		4, 5, 6, 7, UCHAR_MAX,
-		8, 9, 10, 11, UCHAR_MAX,
-		12, 13, 14, 15, UCHAR_MAX,
-		16, 17, 18, 19, UCHAR_MAX,
-		20, 21, 22, 23, UCHAR_MAX
-	};
-	void setupCube() {
-		glGenVertexArrays(1, &cubeVao);
-		glBindVertexArray(cubeVao);
-		glGenBuffers(3, cubeVbo);
-
-		glBindBuffer(GL_ARRAY_BUFFER, cubeVbo[0]);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVerts), cubeVerts, GL_STATIC_DRAW);
-		glVertexAttribPointer((GLuint)0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-		glEnableVertexAttribArray(0);
-
-		glBindBuffer(GL_ARRAY_BUFFER, cubeVbo[1]);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(cubeNorms), cubeNorms, GL_STATIC_DRAW);
-		glVertexAttribPointer((GLuint)1, 3, GL_FLOAT, GL_FALSE, 0, 0);
-		glEnableVertexAttribArray(1);
-
-		glPrimitiveRestartIndex(UCHAR_MAX);
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, cubeVbo[2]);
-		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(cubeIdx), cubeIdx, GL_STATIC_DRAW);
-
-		glBindVertexArray(0);
-		glBindBuffer(GL_ARRAY_BUFFER, 0);
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-
-		cubeShaders[0] = compileShader(FileLoader::LoadString("resources/cube_org.vert").c_str(), GL_VERTEX_SHADER, "cubeVert");
-		cubeShaders[1] = compileShader(FileLoader::LoadString("resources/cube_org.frag").c_str(), GL_FRAGMENT_SHADER, "cubeFrag");
-
-		cubeProgram = glCreateProgram();
-		glAttachShader(cubeProgram, cubeShaders[0]);
-		glAttachShader(cubeProgram, cubeShaders[1]);
-		glBindAttribLocation(cubeProgram, 0, "in_Position");
-		glBindAttribLocation(cubeProgram, 1, "in_Normal");
-		linkProgram(cubeProgram);
-	}
-	void cleanupCube() {
-		glDeleteBuffers(3, cubeVbo);
-		glDeleteVertexArrays(1, &cubeVao);
-
-		glDeleteProgram(cubeProgram);
-		glDeleteShader(cubeShaders[0]);
-		glDeleteShader(cubeShaders[1]);
-	}
-	void updateCube(const glm::mat4& transform) {
-		objMat = transform;
-	}
-	void drawCube() {
-		glEnable(GL_PRIMITIVE_RESTART);
-
-		glBindVertexArray(cubeVao);
-		glUseProgram(cubeProgram);
-		glUniformMatrix4fv(glGetUniformLocation(cubeProgram, "objMat"), 1, GL_FALSE, glm::value_ptr(objMat));
-		glUniformMatrix4fv(glGetUniformLocation(cubeProgram, "mv_Mat"), 1, GL_FALSE, glm::value_ptr(RenderVars::_modelView));
-		glUniformMatrix4fv(glGetUniformLocation(cubeProgram, "mvpMat"), 1, GL_FALSE, glm::value_ptr(RenderVars::_MVP));
-		glUniform4f(glGetUniformLocation(cubeProgram, "color"), .1f, 1.f, 1.f, 0.f);
-		glDrawElements(GL_TRIANGLE_STRIP, numVerts, GL_UNSIGNED_BYTE, 0);
-
-		glUseProgram(0);
-		glBindVertexArray(0);
-		glDisable(GL_PRIMITIVE_RESTART);
-	}
-}
-
 
 #pragma region PHONG
 
@@ -513,11 +391,18 @@ class Object {
 	glm::mat4 objMat;
 
 public:
+
+	glm::vec3 position = { 0,0,0 };
+	glm::vec3 rotation = { 0,0,0 };
+	glm::vec3 scale = { 1,1,1 };
+
 	glm::vec3 colorAmbient = { 0,0,0 };
 	glm::vec3 colorDiffuse = { 1,1,1 };
 	glm::vec3 colorSpecular = { 1,1,1 };
 	glm::float32 specularStrength = 1;
 	glm::float32 normalStrength = 1;
+
+
 
 	Texture albedo;
 	Texture normal;
@@ -614,12 +499,15 @@ public:
 		cleanupObject();
 	}
 
-
-	void updateObject(glm::mat4 matrix) {
-		objMat = matrix;
+	void updateObject() {
+		glm::mat4 t = glm::translate(glm::mat4(), position);
+		glm::mat4 r = glm::toMat4(glm::fquat(glm::radians(rotation)));
+		glm::mat4 s = glm::scale(glm::mat4(), scale);
+		objMat = t * r * s;
 	}
 
 	void drawObject() {
+		updateObject();
 		glBindVertexArray(vao);
 		glUseProgram(program);
 
@@ -686,6 +574,11 @@ public:
 		ImGui::ColorEdit3("Specular", &colorSpecular[0]);
 		ImGui::SliderFloat("Specular Strength", &specularStrength, 0, 2);
 		ImGui::SliderFloat("Normal Strength", &normalStrength, 0.001f, 1.05f, "%.3f", .1f);
+		ImGui::Spacing();
+		ImGui::Text("Transform:");
+		ImGui::DragFloat3("Position", &position.x, 0.01f);
+		ImGui::DragFloat3("Rotation", &rotation.x);
+		ImGui::DragFloat3("Scale", &scale.x, 0.01f);
 		ImGui::End();
 	}
 
@@ -741,13 +634,6 @@ void GLinit(int width, int height) {
 	cubeObj->specular.path = "resources/textures/Metal_SpecularSmoothness.png";
 	cubeObj->setupObject();
 
-	//cubeObj2 = new Object("cube2");
-	//cubeObj2->albedo.path = "resources/textures/Metal_AlbedoTransparency.png";
-	//cubeObj2->albedo.nrChannels = 3;
-	//cubeObj2->normal.path = "resources/textures/Metal_Normal.png";
-	//cubeObj2->normal.nrChannels = 3;
-	//cubeObj2->specular.path = "resources/textures/Metal_SpecularSmoothness.png";
-	//cubeObj2->setupObject();
 
 	lights.push_back(Light());
 
@@ -782,11 +668,6 @@ void GLrender(float dt) {
 		Axis::drawAxis(lights.at(i).position);
 	}
 	{
-		glm::mat4 t = glm::translate(glm::mat4(), glm::vec3(0, 0, 0));
-		glm::mat4 r = glm::mat4(1.f);
-		float size = 1.f;
-		glm::mat4 s = glm::scale(glm::mat4(), glm::vec3(size, size, size));
-		cubeObj->updateObject(t * r * s);
 		cubeObj->drawObject();
 	}
 
